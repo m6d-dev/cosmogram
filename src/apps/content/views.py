80a3babe48d887fb1e1ddc830586a62d1867b181
Muhammad8757 <muhammadjon8757@gmail.com>
2025-10-05 16:52:37 +0500

    ...

--- src/apps/content/views.py
+++ src/apps/content/views.py
@@ -1,14 +1,23 @@
-from rest_framework.viewsets import ModelViewSet
-from drf_spectacular.utils import extend_schema
-from src.apps.content.models.tag import Tag
+from rest_framework.viewsets import ModelViewSet, GenericViewSet
+from drf_spectacular.types import OpenApiTypes
+from drf_spectacular.utils import OpenApiParameter, extend_schema
+from src.apps.content.models.like import Like
+from rest_framework import mixins
 from src.apps.content.models.post import Post
 from src.apps.content.serializers import ListPostSerializer, PostSerializer
 from src.apps.content.services.post import post_service
+from src.apps.content.services.like import like_service
 from src.utils.conts import ViewAction
-
+from rest_framework.response import Response
+from rest_framework import status
+from rest_framework.permissions import IsAuthenticated
+from src.utils.functions import raise_validation_error_detail
+from rest_framework.decorators import action
 
 @extend_schema(tags=["content"])
 class PostAPIView(ModelViewSet):
+    permission_classes = (IsAuthenticated,)
+
     model = Post
 
     def get_serializer_class(self):
@@ -17,4 +26,71 @@ class PostAPIView(ModelViewSet):
         return ListPostSerializer
 
     def get_queryset(self):
-        return post_service.filter(created_by_id=self.request.user.id)
+        return post_service.all()
+    
+
+    def destroy(self, request, *args, **kwargs):
+        instance = self.model.objects.filter(id=kwargs.get("id"), created_by_id=request.user.id)
+        return super().destroy(request, *args, **kwargs)
+
+@extend_schema(tags=["content"])
+class LikeAPIView(GenericViewSet):
+    permission_classes = (IsAuthenticated,)
+    lookup_field = "id"
+    model = Like
+
+
+    def get_queryset(self):
+        return like_service.filter(created_by_id=self.request.user.id)
+
+    @extend_schema(
+        parameters=[
+            OpenApiParameter(
+                name="post_id",
+                type=OpenApiTypes.INT,
+                required=True,
+                location=OpenApiParameter.QUERY
+            )
+        ]
+    )
+    @action(detail=False, methods=["post"], url_path="add")
+    def add_like(self, request, *args, **kwargs):
+        """
+        Custom action to add a like by post_id instead of like_id.
+        """
+        post_id = request.query_params.get("post_id")
+        if not post_id:
+            raise_validation_error_detail("post_id is required")
+
+        like = like_service.exists(post_id=post_id, created_by_id=request.user.id)
+        if like:
+            raise_validation_error_detail("You have liked this post")
+
+        like_service.create(post_id=post_id, created_by_id=request.user.id)
+        return Response(status=status.HTTP_204_NO_CONTENT)
+
+    @extend_schema(
+        parameters=[
+            OpenApiParameter(
+                name="post_id",
+                type=OpenApiTypes.INT,
+                required=True,
+                location=OpenApiParameter.QUERY
+            )
+        ]
+    )
+    @action(detail=False, methods=["delete"], url_path="remove")
+    def remove_like(self, request, *args, **kwargs):
+        """
+        Custom action to delete a like by post_id instead of like_id.
+        """
+        post_id = request.query_params.get("post_id")
+        if not post_id:
+            raise_validation_error_detail("post_id is required")
+
+        like = like_service.filter(post_id=post_id, created_by_id=request.user.id).first()
+        if not like:
+            raise_validation_error_detail("You haven't liked this post")
+
+        like_service.delete(instance=like, post_id=post_id)
+        return Response(status=status.HTTP_204_NO_CONTENT)
